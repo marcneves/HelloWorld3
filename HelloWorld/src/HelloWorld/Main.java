23734eee1e92a681ba7839d418ceefd175ae126d
marcneves <marcneves@209.42.106.211>
2017-01-19 19:51:54 -0500

    hello world 2

---
+++ HelloWorld/src/HelloWorld/Main.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
